9a6ca3ca2bc5e0f815541fe531301c160a6f1967
TophughesBz <119230135+TophughesBz@users.noreply.github.com>
2023-11-09 16:49:46 +0300

    Add files via upload

---
+++ calculator2.py
@@ -0,0 +1,20 @@
+class Calculator:
+    def calculate(self, num1,operator,num2):
+        if operator == '+':
+            return num1 + num2
+        elif operator == '-':
+            return num1 - num2
+        elif operator == '*':
+            return num1 * num2
+        elif operator == '/':
+            if num2 == 0:
+                return "Cannot divide by zero"
+            return num1 / num2
+        else:
+            return "Invalid operator"
+
+num1=int(input("Enter your first number :"))
+operator=(input("Enter your operator :"))
+num2=int(input("Enter your second number :"))
+
+print(f"{num1} {operator} {num2} =",Calculator.calculate(Calculator,num1,operator,num2))
